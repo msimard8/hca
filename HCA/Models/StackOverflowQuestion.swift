ce7bb2b1d8bd6e5186285b59ae0185bbf018b871
Michael <msimard8@gmail.com>
2019-08-20 19:11:04 -0400

    Added a StackOverflowQuestion model struct

---
+++ HCA/Models/StackOverflowQuestion.swift
@@ -0,0 +1,13 @@
+//
+//  StackOverflowQuestion.swift
+//  HCA
+//
+//  Created by Michael Simard on 8/20/19.
+//  Copyright © 2019 Michael Simard. All rights reserved.
+//
+
+import Foundation
+
+struct StackOverflowQuestionModel {
+    
+}
