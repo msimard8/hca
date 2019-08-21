cc298e4ab7dc8893f7566ad2a5392211b430ec2d
Michael <msimard8@gmail.com>
2019-08-21 10:57:38 -0400

    Added app key so i dont have tow orry about quota

--- HCA/Network/NetworkService.swift
+++ HCA/Network/NetworkService.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class NetworkService: NSObject {
     let baseURL = "https://api.stackexchange.com/2.2"
+    var key = ""
     let session = URLSession(configuration: .default)
     var dataTask: URLSessionDataTask?
     var decoder = JSONDecoder()
@@ -24,7 +25,13 @@ class NetworkService: NSObject {
         dataTask?.cancel()
 
          if var urlComponents = URLComponents (string: "\(baseURL)/search/advanced") {
-            urlComponents.query = "page=\(page)&pagesize=10&todate=\(Int(date.timeIntervalSince1970))&order=desc&sort=creation&accepted=True&answers=2&site=stackoverflow&filter=!4(Yr(ztczR*6OAdo1"
+            urlComponents.query = "page=\(page)&pagesize=10"
+            + "&todate=\(Int(date.timeIntervalSince1970))"
+            + "&order=desc&sort=creation"
+            + "&accepted=True&answers=2"
+            + "&site=stackoverflow"
+            + "&filter=!4(Yr(ztczR*6OAdo1"
+            + "&key=wV5REhLr1WpnH1aejgbZHw(("
 
             guard let url = urlComponents.url else {
                 return
@@ -42,8 +49,10 @@ class NetworkService: NSObject {
                     let data = data,
                     let response = response as? HTTPURLResponse,
                     response.statusCode == 200 {
-                    let questions = try! JSONDecoder().decode(StackOverflowQuestionList.self, from: data)
-                    completion(questions)
+                    let questions = try! self?.decoder.decode(StackOverflowQuestionList.self, from: data)
+                    completion(questions!)
+                    print (questions!.quotaRemaining)
+
                 }
                 else {
                     //assume error
