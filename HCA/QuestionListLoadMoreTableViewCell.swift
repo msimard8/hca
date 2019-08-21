3825fafea54e45b4390f73a6ebcfa4c44e7892b8
Michael <msimard8@gmail.com>
2019-08-21 09:47:16 -0400

    new files added, a cell that indicates more questions are loading, and the QuestionTableViewCell

---
+++ HCA/QuestionListLoadMoreTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  QuestionListLoadMoreTableViewCell.swift
+//  HCA
+//
+//  Created by Michael Simard on 8/20/19.
+//  Copyright © 2019 Michael Simard. All rights reserved.
+//
+
+import UIKit
+
+class QuestionListLoadMoreTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
