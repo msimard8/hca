74e37ae5cb797c273af1e0d39430740bf32c150e
Michael <msimard8@gmail.com>
2019-08-21 16:27:18 -0400

    added a utils class

---
+++ HCA/Util/Utils.swift
@@ -0,0 +1,13 @@
+//
+//  Utils.swift
+//  HCA
+//
+//  Created by Michael Simard on 8/21/19.
+//  Copyright © 2019 Michael Simard. All rights reserved.
+//
+
+import UIKit
+
+class Utils: NSObject {
+
+}
