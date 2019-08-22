3f51e6dd3e5c3d7ee81ce8de90123e7f6cb09af4
Michael <msimard8@gmail.com>
2019-08-21 22:39:46 -0400

    render a tableview of question and answers

--- HCA/AnswerListViewController.swift
+++ HCA/AnswerListViewController.swift
@@ -7,24 +7,77 @@
 //
 
 import UIKit
+import WebKit
 
 class AnswerListViewController: UIViewController {
+
+    @IBOutlet weak var answersTableView: UITableView?
+
+
+    var question : StackOverflowQuestion? {
+        didSet {
+           // answers.append(StackOverflowAnswer(score: 0, isAccepted: true, answerId: 3, creationDate: Date(), body: "boo"))
+            NetworkService.shared.getAnswers(questionId: question?.questionId ?? 0) { (answerList) in
+                self.answers = answerList.answers
+                DispatchQueue.main.async {
+                    self.answersTableView?.reloadData()
+                }
+            }
+        }
+    }
+    var answers = [StackOverflowAnswer]()
+
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        answersTableView?.estimatedRowHeight = 55
+        answersTableView?.rowHeight = UITableView.automaticDimension
+
+        answersTableView?.register(UINib(nibName: "AnswerListQuestionTableViewCell", bundle: nil), forCellReuseIdentifier: AnswerListQuestionTableViewCell.identifier)
+
+        answersTableView?.register(UINib(nibName: "AnswerListAnswerTableViewCell", bundle: nil), forCellReuseIdentifier: AnswerListAnswerTableViewCell.identifier)
         // Do any additional setup after loading the view.
     }
 
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.title = "Answers"
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
     }
-    */
+
+
+}
+
+extension AnswerListViewController : UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return answers.count + 1
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+
+            if indexPath.row == 0 {
+                guard let answerListQuestionCell = tableView.dequeueReusableCell(withIdentifier: AnswerListQuestionTableViewCell.identifier, for: indexPath) as? AnswerListQuestionTableViewCell else{
+                    return UITableViewCell()
+                }
+                answerListQuestionCell.question = question
+                return answerListQuestionCell
+            }
+            else {
+                guard let answerListAnswerCell = tableView.dequeueReusableCell(withIdentifier: AnswerListAnswerTableViewCell.identifier, for: indexPath) as? AnswerListAnswerTableViewCell else{
+                    return UITableViewCell()
+                }
+               answerListAnswerCell.answer = answers[indexPath.row - 1]
+                return answerListAnswerCell
+
+            }
+
+    }
+
+
+}
+
+extension AnswerListViewController : UITableViewDelegate {
 
 }
