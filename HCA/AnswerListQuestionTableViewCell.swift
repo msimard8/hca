32c8f8b1f319dac4f870672ed23fcc6844e8aa69
Michael <msimard8@gmail.com>
2019-08-21 22:37:21 -0400

    Added a few more files, a cell that represents an answer, a cell that represnts a question on the answers screen, an extension file for NSAttributedString, an extension file for UIView, and a StackOverflowAnswer and StackOverflowAnswerList model

---
+++ HCA/AnswerListQuestionTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  AnswerListQuestionTableViewCell.swift
+//  HCA
+//
+//  Created by Michael Simard on 8/21/19.
+//  Copyright © 2019 Michael Simard. All rights reserved.
+//
+
+import UIKit
+
+class AnswerListQuestionTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ HCA/Models/StackOverflowAnswer.swift
@@ -0,0 +1,9 @@
+//
+//  StackOverflowAnswer.swift
+//  HCA
+//
+//  Created by Michael Simard on 8/21/19.
+//  Copyright © 2019 Michael Simard. All rights reserved.
+//
+
+import Foundation
