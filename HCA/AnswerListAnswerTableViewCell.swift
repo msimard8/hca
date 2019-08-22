385b5970ce67c217b1a108160a6f28cf9dcab71a
Michael <msimard8@gmail.com>
2019-08-21 22:38:14 -0400

    render answer into answer cell using attributedlabels

--- HCA/AnswerListAnswerTableViewCell.swift
+++ HCA/AnswerListAnswerTableViewCell.swift
@@ -10,9 +10,22 @@ import UIKit
 
 class AnswerListAnswerTableViewCell: UITableViewCell {
 
+    @IBOutlet weak var answerAttributedLabel: UILabel!
+    
+    var answer:StackOverflowAnswer? {
+        didSet {
+            let font = UIFont.systemFont(ofSize: 18)
+            let attributedString = NSMutableAttributedString.init(html: answer?.body ?? "")
+            answerAttributedLabel.numberOfLines = 0
+            attributedString?.setFontFace(font: font, color: .black)
+            answerAttributedLabel.attributedText = attributedString
+        }
+    }
+    static let identifier = "AnswerListAnswerTableViewCell"
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
