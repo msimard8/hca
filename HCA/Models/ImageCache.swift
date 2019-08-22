f382ae30012061b9a80099c78e74176b389e16db
Michael <msimard8@gmail.com>
2019-08-22 17:17:57 -0400

    Added an image cache for thumbnails of profile pics

--- HCA/Models/ImageCache.swift
+++ HCA/Models/ImageCache.swift
@@ -10,4 +10,26 @@ import UIKit
 
 class ImageCache: NSObject {
 
+    private let thumbnailCache = NSCache<NSString, UIImage>()
+
+    internal static var shared: ImageCache = {
+        let instance = ImageCache()
+        return instance
+    }()
+
+    func setImageCap(cap: Int) {
+        thumbnailCache.countLimit = cap
+    }
+
+    func removeImages() {
+        thumbnailCache.removeAllObjects()
+    }
+
+    func storeImage(key: String, image: UIImage) {
+        thumbnailCache.setObject(image, forKey: key as NSString)
+    }
+
+    func retrieveImage(key: String) -> UIImage? {
+        return thumbnailCache.object(forKey: key as NSString)
+    }
 }
