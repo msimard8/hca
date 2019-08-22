add9e1177040f50aa896755a6c612339624a0845
Michael <msimard8@gmail.com>
2019-08-22 17:12:46 -0400

    updated app icon, added an ImageCache class as well as a StackOverflowOwner model

---
+++ HCA/Models/ImageCache.swift
@@ -0,0 +1,13 @@
+//
+//  ImageCache.swift
+//  HCA
+//
+//  Created by Michael Simard on 8/22/19.
+//  Copyright © 2019 Michael Simard. All rights reserved.
+//
+
+import UIKit
+
+class ImageCache: NSObject {
+
+}
---
+++ HCA/Models/StackOverflowOwner.swift
@@ -0,0 +1,9 @@
+//
+//  StackOverflowOwner.swift
+//  HCA
+//
+//  Created by Michael Simard on 8/22/19.
+//  Copyright © 2019 Michael Simard. All rights reserved.
+//
+
+import Foundation
