4747245009b15a433fc4ce304b018656031765b3
Michael <msimard8@gmail.com>
2019-08-21 22:41:51 -0400

    Added an extension that converts html (answer body) into an attributed string, also added an extension that can resize attributed string created from html, thanks stackoverflow

--- HCA/Extension/NSAttributedString+Extensions.swift
+++ HCA/Extension/NSAttributedString+Extensions.swift
@@ -7,3 +7,56 @@
 //
 
 import Foundation
+import UIKit
+
+extension NSAttributedString {
+
+    internal convenience init?(html: String) {
+        guard let data = html.data(using: String.Encoding.utf16, allowLossyConversion: false) else {
+            return nil
+        }
+
+        guard let attributedString = try?  NSAttributedString(data: data, options: [.documentType: NSAttributedString.DocumentType.html, .characterEncoding: String.Encoding.utf8.rawValue], documentAttributes: nil) else {
+            return nil
+        }
+
+        self.init(attributedString: attributedString)
+    }
+}
+
+
+extension NSMutableAttributedString {
+    func setFontFace(font: UIFont, color: UIColor? = nil) {
+        beginEditing()
+        self.enumerateAttribute(
+            .font,
+            in: NSRange(location: 0, length: self.length)
+        ) { (value, range, stop) in
+
+            if let f = value as? UIFont,
+                let newFontDescriptor = f.fontDescriptor
+                    .withFamily(font.familyName)
+                    .withSymbolicTraits(f.fontDescriptor.symbolicTraits) {
+
+                let newFont = UIFont(
+                    descriptor: newFontDescriptor,
+                    size: font.pointSize
+                )
+                removeAttribute(.font, range: range)
+                addAttribute(.font, value: newFont, range: range)
+                if let color = color {
+                    removeAttribute(
+                        .foregroundColor,
+                        range: range
+                    )
+                    addAttribute(
+                        .foregroundColor,
+                        value: color,
+                        range: range
+                    )
+                }
+            }
+        }
+        endEditing()
+    }
+}
