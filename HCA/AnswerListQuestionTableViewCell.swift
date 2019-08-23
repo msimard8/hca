a838f5518b158c7151457969d3b354591b95598b
Michael <msimard8@gmail.com>
2019-08-23 10:14:22 -0400

    add body to question card in the answer list

--- HCA/AnswerListQuestionTableViewCell.swift
+++ HCA/AnswerListQuestionTableViewCell.swift
@@ -13,6 +13,7 @@ class AnswerListQuestionTableViewCell: UITableViewCell {
 
     @IBOutlet weak var questionAttributedLabel: UILabel!
 
+    @IBOutlet weak var bodyAttributedLabel: UILabel!
     @IBOutlet weak var nameLabel: UILabel!
 
     @IBOutlet weak var profileImageView: UIImageView!
@@ -21,21 +22,34 @@ class AnswerListQuestionTableViewCell: UITableViewCell {
     var question:StackOverflowQuestion? {
         didSet {
 
-            let font = UIFont.systemFont(ofSize: 24)
+
+            dateLabel.text = Utils.formatDate(date: question?.creationDate ?? Date())
+            nameLabel.text = question?.owner.displayName ?? "anonymous"
+
+
+            let font = UIFont.boldSystemFont(ofSize: 24)
 
             let attributes: [NSAttributedString.Key: Any] = [
                 .foregroundColor: UIColor.white,
                 .font: font
             ]
-
-
-            dateLabel.text = Utils.formatDate(date: question?.creationDate ?? Date())
-            nameLabel.text = question?.owner.displayName ?? "anonymous"
             let attributedString = NSMutableAttributedString.init(html: question?.title ?? "")
-
             questionAttributedLabel.numberOfLines = 0
             attributedString?.addAttributes(attributes, range: NSRange(location: 0, length: (attributedString?.length ?? 0)))
             questionAttributedLabel.attributedText = attributedString
+
+
+            let bodyFont = UIFont.systemFont(ofSize: 18)
+
+            let bodyAttributes: [NSAttributedString.Key: Any] = [
+                .foregroundColor: UIColor.white,
+                .font: bodyFont
+            ]
+
+            let bodyAttributedString = NSMutableAttributedString.init(html: question?.body ?? "")
+            bodyAttributedLabel.numberOfLines = 0
+            bodyAttributedString?.addAttributes(bodyAttributes, range: NSRange(location: 0, length: (bodyAttributedString?.length ?? 0)))
+            bodyAttributedLabel.attributedText = bodyAttributedString
         }
     }
 
