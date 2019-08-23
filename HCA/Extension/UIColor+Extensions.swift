88dd84bb728480e02f56a7207725b0673aa515d8
Michael <msimard8@gmail.com>
2019-08-23 16:50:34 -0400

    made an extension for custom colors

---
+++ HCA/Extension/UIColor+Extensions.swift
@@ -0,0 +1,25 @@
+//
+//  UIColor+Extensions.swift
+//  HCA
+//
+//  Created by Michael Simard on 8/23/19.
+//  Copyright © 2019 Michael Simard. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+
+    struct Primary {
+        static let Blue =  UIColor(red: 0,
+                                   green: 0.4118,
+                                   blue: 0.8588,
+                                   alpha: 1.0)
+    }
+    struct Secondary {
+        static let Blue = UIColor(red: 187.0/255.0,
+                                  green: 224.0/255.0,
+                                  blue: 255.0/255.0,
+                                  alpha: 1.0)
+    }
+}
