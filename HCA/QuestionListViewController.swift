3d032f87da6f70028a44cb701bdb0e1cc3ff9918
Michael Simard <msimard@learninga-z.com>
2019-08-26 14:13:51 -0400

    fixed a typo, removed black background from api alert popup

--- HCA/QuestionListViewController.swift
+++ HCA/QuestionListViewController.swift
@@ -133,11 +133,10 @@ class QuestionListViewController: UIViewController {
     }
     
     func verifyAPIKey(){
-        if !NetworkService.shared.hasAPIKeyBeenReplaced() && hasAPIAlertBeenDisplayed{
+        if !NetworkService.shared.hasAPIKeyBeenReplaced() && !hasAPIAlertBeenDisplayed{
         hasAPIAlertBeenDisplayed = true
         let message = "Please go to NetworkService.swift and edit line 13 to have the API key I gave you. The app will still work but the API request quota may be reached."
         let alert = UIAlertController(title: "Please set API key", message: message, preferredStyle:.alert)
-        alert.view.backgroundColor = UIColor.black
         alert.view.layer.cornerRadius = 15
         alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { action in
             alert.dismiss(animated: true, completion: {
