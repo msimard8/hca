e5ef5259bb3b2d60384f4ebf38d33a59dd562498
Michael <msimard8@gmail.com>
2019-08-26 10:58:11 -0400

    updated icon

---
+++ HCA/LogoView.swift
@@ -0,0 +1,21 @@
+//
+//  LogoView.swift
+//  HCA
+//
+//  Created by Michael Simard on 8/26/19.
+//  Copyright © 2019 Michael Simard. All rights reserved.
+//
+
+import UIKit
+
+class LogoView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
